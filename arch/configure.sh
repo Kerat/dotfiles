b7fbcd92c582e30f280cb09fe885ca6ee6d3ff5e
Kerat <tarek.marce@gmail.com>
2020-04-27 00:53:33 +0200

    Arch configuration script

---
+++ arch/configure.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Azerty layout for X11
+cat > /etc/X11/xorg.conf.d/20-keyboard.conf <<EOF
+Section "InputClass"
+    Identifier "keyboard"
+    MatchIsKeyboard "yes"
+    Option "XkbLayout" "fr"
+EndSection 
+EOF
+
+# Activate Lightdm
+systemctl enable lightdm.service
+
+# Activate networkmanager
+systemctl enable networkmanager.service
+
